4e53f660fa73b6e7005a9532c4200261485319d9
Stéphane Nicolas <steff.nicolas@gmail.com>
2014-08-22 08:45:00 -0400

    Refactor tests on fragments and add more.

---
+++ injectview-plugin/src/test/java/com/github/stephanenicolas/injectview/InjectViewProcessorWithActivityTest.java
@@ -0,0 +1,226 @@
+package com.github.stephanenicolas.injectview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.is;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import org.robolectric.Robolectric;
+
+/**
+ * These tests are really complex to setup.
+ * Take your time for maintenance.
+ * @author SNI
+ */
+@RunWith(InjectViewTestRunner.class)
+public class InjectViewProcessorWithActivityTest {
+  public static final String VIEW_TAG = "TAG";
+  public static final int VIEW_ID = 101;
+  public static final int CONTENT_VIEW_ID = 100;
+
+  private InjectViewProcessor processor = new InjectViewProcessor();
+
+  @Test
+  public void shouldInjectView_whenUsingSetContentView_withId() {
+    TestActivityWithId activity = Robolectric.buildActivity(TestActivityWithId.class)
+        .create()
+        .get();
+    assertNotNull(activity.text1);
+    assertThat(activity.text1.getId(), is(VIEW_ID));
+  }
+
+  @Test
+  public void shouldInjectView_whenUsingSetContentView_withTag() {
+    TestActivityWithTag activity = Robolectric.buildActivity(TestActivityWithTag.class)
+        .create()
+        .get();
+    assertNotNull(activity.text1);
+    assertThat((String) activity.text1.getTag(), is(VIEW_TAG));
+  }
+
+  @Test
+  public void shouldInjectView_whenUsingAtContentView_withId() {
+    TestActivityWithContentViewAndId activity = Robolectric.buildActivity(TestActivityWithContentViewAndId.class)
+        .create()
+        .get();
+    assertNotNull(activity.text1);
+    assertThat(activity.text1.getId(), is(VIEW_ID));
+    //http://stackoverflow.com/a/8817003/693752
+    View root = ((ViewGroup) activity.findViewById(android.R.id.content)).getChildAt(0);
+    assertNotNull(root);
+    assertThat((Integer) root.getTag(), is(CONTENT_VIEW_ID));
+  }
+
+  @Test
+  public void shouldInjectView_whenUsingAtContentView_withTag() {
+    TestActivityWithContentViewAndTag activity = Robolectric.buildActivity(TestActivityWithContentViewAndTag.class)
+        .create()
+        .get();
+    assertNotNull(activity.text1);
+    assertThat((String) activity.text1.getTag(), is(VIEW_TAG));
+    //http://stackoverflow.com/a/8817003/693752
+    View root = ((ViewGroup) activity.findViewById(android.R.id.content)).getChildAt(0);
+    assertNotNull(root);
+    assertThat((Integer) root.getTag(), is(CONTENT_VIEW_ID));
+  }
+
+  @Test
+  public void shouldInjectView_whenUsingAtContentView_withId_withoutOnCreate() {
+    TestActivityWithContentViewAndIdWithoutOnCreate activity = Robolectric.buildActivity(TestActivityWithContentViewAndIdWithoutOnCreate.class)
+        .create()
+        .get();
+    assertNotNull(activity.text1);
+    assertThat(activity.text1.getId(), is(VIEW_ID));
+    //http://stackoverflow.com/a/8817003/693752
+    View root = ((ViewGroup) activity.findViewById(android.R.id.content)).getChildAt(0);
+    assertNotNull(root);
+    assertThat((Integer) root.getTag(), is(CONTENT_VIEW_ID));
+  }
+
+  @Test
+  public void shouldInjectView_whenUsingAtContentView_withTag_withoutOnCreate() {
+    TestActivityWithContentViewAndTagWithoutOnCreate activity = Robolectric.buildActivity(TestActivityWithContentViewAndTagWithoutOnCreate.class)
+        .create()
+        .get();
+    assertNotNull(activity.text1);
+    assertThat((String) activity.text1.getTag(), is(VIEW_TAG));
+    //http://stackoverflow.com/a/8817003/693752
+    View root = ((ViewGroup) activity.findViewById(android.R.id.content)).getChildAt(0);
+    assertNotNull(root);
+    assertThat((Integer) root.getTag(), is(CONTENT_VIEW_ID));
+  }
+
+
+
+  public static class TestActivityWithId extends Activity {
+    @InjectView(VIEW_ID)
+    protected TextView text1;
+
+    protected LinearLayout root;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      root = new LinearLayout(this);
+      setContentView(root);
+    }
+
+    @Override public View findViewById(int id) {
+      final TextView text1 = new TextView(this);
+      root.addView(text1);
+      text1.setId(id);
+      return text1;
+    }
+  }
+
+  public static class TestActivityWithTag extends Activity {
+    @InjectView(tag=VIEW_TAG)
+    protected TextView text1;
+
+    protected LinearLayout root;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      root = new LinearLayout(this);
+      final TextView text = new TextView(this);
+      root.addView(text);
+      text.setTag(VIEW_TAG);
+      setContentView(root);
+    }
+
+  }
+
+  @ContentView(CONTENT_VIEW_ID)
+  public static class TestActivityWithContentViewAndId extends Activity {
+    @InjectView(VIEW_ID)
+    protected TextView text1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+    }
+
+    @Override public View findViewById(int id) {
+      if (id==VIEW_ID) {
+        final TextView text1 = new TextView(this);
+        text1.setId(id);
+        return text1;
+      } else {
+        return super.findViewById(id);
+      }
+    }
+
+    @Override public void setContentView(int layoutResID) {
+      LinearLayout layout = new LinearLayout(this);
+      layout.setTag(CONTENT_VIEW_ID);
+      setContentView(layout);
+    }
+  }
+
+  @ContentView(CONTENT_VIEW_ID)
+  public static class TestActivityWithContentViewAndTag extends Activity {
+    @InjectView(tag=VIEW_TAG)
+    protected TextView text1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+    }
+
+    @Override public void setContentView(int layoutResID) {
+      LinearLayout linearLayout = new LinearLayout(this);
+      linearLayout.setTag(CONTENT_VIEW_ID);
+      final TextView text = new TextView(this);
+      text.setTag(VIEW_TAG);
+      linearLayout.addView(text);
+      setContentView(linearLayout);
+    }
+  }
+
+
+  @ContentView(CONTENT_VIEW_ID)
+  public static class TestActivityWithContentViewAndIdWithoutOnCreate extends Activity {
+    @InjectView(VIEW_ID)
+    protected TextView text1;
+
+    @Override public View findViewById(int id) {
+      if (id==VIEW_ID) {
+        final TextView text1 = new TextView(this);
+        text1.setId(id);
+        return text1;
+      } else {
+        return super.findViewById(id);
+      }
+    }
+
+    @Override public void setContentView(int layoutResID) {
+      LinearLayout layout = new LinearLayout(this);
+      layout.setTag(CONTENT_VIEW_ID);
+      setContentView(layout);
+    }
+  }
+
+  @ContentView(CONTENT_VIEW_ID)
+  public static class TestActivityWithContentViewAndTagWithoutOnCreate extends Activity {
+    @InjectView(tag=VIEW_TAG)
+    protected TextView text1;
+
+    @Override public void setContentView(int layoutResID) {
+      LinearLayout linearLayout = new LinearLayout(this);
+      linearLayout.setTag(CONTENT_VIEW_ID);
+      final TextView text = new TextView(this);
+      text.setTag(VIEW_TAG);
+      linearLayout.addView(text);
+      setContentView(linearLayout);
+    }
+  }
+}
---
+++ injectview-plugin/src/test/java/com/github/stephanenicolas/injectview/InjectViewProcessorWithFragmentTest.java
@@ -0,0 +1,110 @@
+package com.github.stephanenicolas.injectview;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+/**
+ * These tests are really complex to setup.
+ * Take your time for maintenance.
+ * @author SNI
+ */
+@RunWith(InjectViewTestRunner.class)
+public class InjectViewProcessorWithFragmentTest {
+  public static final String FRAGMENT_TAG = "TAG";
+  public static final String FRAGMENT_TAG2 = "TAG2";
+  public static final int VIEW_ID = 101;
+
+  private InjectViewProcessor processor = new InjectViewProcessor();
+
+  @Test
+  public void shouldInjectView_whenUsingAtContentView_withId_withoutOnViewCreated() {
+    TestActivity
+        activity = Robolectric.buildActivity(TestActivity.class)
+        .create()
+        .get();
+    assertNotNull(activity.testFragmentWithoutOnViewCreated.text1);
+    assertThat(activity.testFragmentWithoutOnViewCreated.text1.getId(), is(VIEW_ID));
+  }
+
+  @Test
+  public void shouldInjectView_whenUsingAtContentView_withId_withOnViewCreated() {
+    TestActivity
+        activity = Robolectric.buildActivity(TestActivity.class)
+        .create()
+        .get();
+    assertNotNull(activity.testFragmentWithOnViewCreated.text1);
+    assertThat(activity.testFragmentWithOnViewCreated.text1.getId(), is(VIEW_ID));
+  }
+
+
+
+  public static class TestActivity extends Activity {
+    TestFragmentWithoutOnViewCreated testFragmentWithoutOnViewCreated = new TestFragmentWithoutOnViewCreated();
+    TestFragmentWithOnViewCreated testFragmentWithOnViewCreated = new TestFragmentWithOnViewCreated();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      getFragmentManager().beginTransaction().add(testFragmentWithoutOnViewCreated, FRAGMENT_TAG).commit();
+      getFragmentManager().beginTransaction().add(testFragmentWithOnViewCreated, FRAGMENT_TAG2).commit();
+    }
+
+    @Override
+    public View findViewById(int id) {
+      if (id==VIEW_ID) {
+        final TextView text1 = new TextView(this);
+        text1.setId(id);
+        return text1;
+      } else {
+        return super.findViewById(id);
+      }
+    }
+  }
+
+  public static class TestFragmentWithoutOnViewCreated extends Fragment {
+    @InjectView(VIEW_ID)
+    public TextView text1;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState) {
+      LinearLayout layout = new LinearLayout(getActivity());
+      final TextView text1 = new TextView(getActivity());
+      text1.setId(VIEW_ID);
+      layout.addView(text1);
+      return layout;
+    }
+  }
+
+  public static class TestFragmentWithOnViewCreated extends Fragment {
+    @InjectView(VIEW_ID)
+    public TextView text1;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState) {
+      LinearLayout layout = new LinearLayout(getActivity());
+      final TextView text1 = new TextView(getActivity());
+      text1.setId(VIEW_ID);
+      layout.addView(text1);
+      return layout;
+    }
+
+    @Override public void onViewCreated(View view, Bundle savedInstanceState) {
+      super.onViewCreated(view, savedInstanceState);
+    }
+  }
+}
