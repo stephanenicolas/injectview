4c4effe2a25c445af383d0d027edab7632d83b6a
Stéphane Nicolas <steff.nicolas@gmail.com>
2014-08-22 13:15:13 -0400

    More tests for fragment injection in activities

--- injectview-plugin/src/test/java/com/github/stephanenicolas/injectview/InjectViewProcessorForFragmentsWithActivityTest.java
+++ injectview-plugin/src/test/java/com/github/stephanenicolas/injectview/InjectViewProcessorForFragmentsWithActivityTest.java
@@ -3,6 +3,7 @@ package com.github.stephanenicolas.injectview;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.os.Bundle;
 import android.widget.LinearLayout;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,6 +32,17 @@ public class InjectViewProcessorForFragmentsWithActivityTest {
 
   @Test
   public void shouldInjectFragment_whenUsingSetContentView() {
+    TestActivityWithoutContentView activity = Robolectric.buildActivity(TestActivityWithoutContentView.class)
+        .create()
+        .get();
+    assertNotNull(activity.fragment);
+    assertThat(activity.fragment.getId(), is(VIEW_ID));
+    assertNotNull(activity.fragment2);
+    assertThat(activity.fragment2.getTag(), is(VIEW_TAG2));
+  }
+
+  @Test
+  public void shouldInjectFragment_whenUsingContentView() {
     TestActivityWithContentView activity = Robolectric.buildActivity(TestActivityWithContentView.class)
         .create()
         .get();
@@ -40,6 +52,40 @@ public class InjectViewProcessorForFragmentsWithActivityTest {
     assertThat(activity.fragment2.getTag(), is(VIEW_TAG2));
   }
 
+
+  public static class TestActivityWithoutContentView extends Activity {
+    @InjectFragment(VIEW_ID)
+    protected Fragment fragment;
+    @InjectFragment(tag=VIEW_TAG2)
+    protected Fragment fragment2;
+
+    protected LinearLayout root;
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      root = new LinearLayout(this);
+      root.setId(CONTENT_VIEW_ID);
+      LinearLayout fragmentHost = new LinearLayout(this);
+      fragmentHost.setId(VIEW_ID);
+      root.addView(fragmentHost);
+      LinearLayout fragmentHost2 = new LinearLayout(this);
+      fragmentHost2.setId(VIEW_ID2);
+      root.addView(fragmentHost2);
+      setContentView(root);
+    }
+
+    @Override public FragmentManager getFragmentManager() {
+      FragmentManager fragmentManager = super.getFragmentManager();
+      if (fragmentManager.findFragmentById(VIEW_ID) == null ) {
+        fragmentManager.beginTransaction().add(VIEW_ID, new DummyFragment(), VIEW_TAG).commit();
+      }
+      if (fragmentManager.findFragmentByTag(VIEW_TAG2) == null ) {
+        fragmentManager.beginTransaction().add(VIEW_ID2, new DummyFragment(), VIEW_TAG2).commit();
+      }
+      fragmentManager.executePendingTransactions();
+      return fragmentManager;
+    }
+  }
 
   @ContentView(CONTENT_VIEW_ID)
   public static class TestActivityWithContentView extends Activity {
